0cd7b0eb247781e86e08a89b41646d9b008289a9
Olacgk <theotimechognika@gmail.com>
2022-03-11 11:15:45 -0800

    2.I sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  char l;
+
+  for ( l='a'; l <= 'z'; l++)
+    {
+      putchar(l);
+      printf("\n");
+    }
+
+   return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,2 +1,3 @@
 0. a program that prints whether the number stored in the variable is positive or negative  
 1. a program that print the last digit of the number stored in the variable
+2. a program that prints the alphabet in lowercase
